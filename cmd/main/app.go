a635ba1612d79558bd3b1be068907d16606bcce0
dmitro <dima25122018@gmail.com>
2024-09-08 16:19:39 +0300

    added configuration, socket and refactored cmd/main/app

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"rest-api-practice/internal/config"
 	"rest-api-practice/internal/user"
 	"rest-api-practice/pkg/logging"
 	"time"
+)
+
+const (
+	socket        = "sock"
+	AppSocketFile = "app.sock"
 )
 
 func main() {
@@ -14,20 +24,51 @@ func main() {
 	logger.Info("create logger")
 	router := httprouter.New()
 
+	cfg := config.GetConfig()
+
 	logger.Info("register user handler")
 	handler := user.NewHandler(logger)
 	handler.Register(router)
 
-	start(router)
+	start(router, cfg)
 }
 
-func start(router *httprouter.Router) {
+func start(router *httprouter.Router, cfg *config.Config) {
 	logger := logging.GetLogger()
-
 	logger.Info("start application")
-	listener, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		panic(err)
+
+	var listener net.Listener
+	var listenErr error
+
+	if cfg.Listen.Type == socket {
+		logger.Info("detect app path")
+
+		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		logger.Info("create socket")
+		socketPath := path.Join(appDir, AppSocketFile)
+
+		if _, err = os.Stat(socketPath); err == nil {
+			logger.Info("removing existing socket file")
+			if err = os.Remove(socketPath); err != nil {
+				logger.Fatal("failed to remove existing socket file: ", err)
+			}
+		}
+
+		logger.Info("listen unix socket")
+		listener, listenErr = net.Listen("unix", socketPath)
+		logger.Infof("server is listening unix socket: %s", socketPath)
+	} else {
+		logger.Info("listen tcp")
+		listener, listenErr = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIp, cfg.Listen.Port))
+		logger.Infof("server is listening on port %s:%s", cfg.Listen.BindIp, cfg.Listen.Port)
+	}
+
+	if listenErr != nil {
+		logger.Fatal(listenErr)
 	}
 
 	server := &http.Server{
@@ -36,6 +77,5 @@ func start(router *httprouter.Router) {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	logger.Info("server is listening on port 0,0,0,0:8080")
 	logger.Fatal(server.Serve(listener))
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module rest-api-practice
 go 1.23.1
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
