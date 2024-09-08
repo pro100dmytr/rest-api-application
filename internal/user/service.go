465009c28f7a6bb61ab0be0602a521a146743b0e
dmitro <dima25122018@gmail.com>
2024-09-08 19:06:43 +0300

    added part of the mongodb implementation

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,15 @@
 package user
+
+import (
+	"context"
+	"rest-api-practice/pkg/logging"
+)
+
+type Service struct {
+	storage Storage
+	logger  *logging.Logger
+}
+
+func (s *Service) Create(ctx context.Context, dto CreateUserDTO) (u User, err error) {
+	return
+}
